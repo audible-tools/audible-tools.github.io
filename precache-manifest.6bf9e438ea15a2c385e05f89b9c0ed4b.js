16c4d2a226898520fc7c896c511b2dd61e8ec87c
Jonas Kamsker <git.jonas@kamsker.at>
2020-08-05 00:16:01 +0200

    1

---
+++ precache-manifest.6bf9e438ea15a2c385e05f89b9c0ed4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf5b008ceeb14a65200399a40976641",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f6f8c323730923bc31d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "612eba33572c03bde608",
+    "url": "/static/js/2.44826a83.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.44826a83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f6f8c323730923bc31d",
+    "url": "/static/js/main.dd06e156.chunk.js"
+  },
+  {
+    "revision": "d271f6cd182fc1f92a38",
+    "url": "/static/js/runtime-main.e9d18258.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
