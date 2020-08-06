d16fb382cd79ac8b9c40a62812c4d2a45d379440
Jonas Kamsker <jonas.kamsker@lints.at>
2020-08-06 10:54:49 +0200

    stuff

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import ChecksumResolver from './ChecksumResolver';
 
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-174657678-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 ReactDOM.render(
   <ChecksumResolver />,
   document.getElementById('root')
